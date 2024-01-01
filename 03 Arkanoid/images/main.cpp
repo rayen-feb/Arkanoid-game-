3f71a958134c012abff6848efbf746ba229d4c73
AHMED RAYEN <131598929+rayen-feb@users.noreply.github.com>
2024-01-01 08:08:01 -1100

    Update main.cpp
    loading images

--- 03 Arkanoid/images/main.cpp
+++ 03 Arkanoid/images/main.cpp
@@ -8,3 +8,9 @@ int main()
 
     RenderWindow app(VideoMode(520, 450), "Arkanoid!");
     app.setFramerateLimit(60);
+  Texture t1,t2,t3,t4;
+    t1.loadFromFile("images/block01.png");
+    t2.loadFromFile("images/background.jpg");
+    t3.loadFromFile("images/ball.png");
+    t4.loadFromFile("images/paddle.png");
+
